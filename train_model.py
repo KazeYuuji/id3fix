b3efe509adb8b5669982a129550d48a7b974a9f4
KazeYuuji <zinhalcrafter@gmail.com>
2025-12-01 07:00:36 +0700

    wine

--- train_model.py
+++ train_model.py
@@ -17,7 +17,7 @@ df_disc[features] = disc.fit_transform(df_disc[features])
 X = df_disc.drop(['quality', 'Id'], axis=1)
 y = df_disc['quality']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 id3_model = DecisionTreeClassifier(criterion='entropy', max_depth=5)
 id3_model.fit(X_train, y_train)
